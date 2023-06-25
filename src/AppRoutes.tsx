5f2ba0fe32c6393a9fef96f8ddb60c14ec625763
Endy-Guff <andreymaranchak@yandex.ru>
2023-06-26 00:50:51 +0300

    ff14

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Routes } from 'react-router-dom';
+import {Route, Routes, Navigate} from 'react-router-dom';
 import {Main} from "./components/Main/Main";
 import {Chat} from "./components/Chat/Chat";
 import {ThemeType} from "./App";
@@ -9,6 +9,7 @@ export const AppRoutes = (props:{theme: ThemeType}) => {
         <Routes>
             <Route path={'/'} element={<Main theme={props.theme} />}/>
             <Route path={'/chat'} element={<Chat theme={props.theme} />}/>
+            <Route path={'*'} element={<Navigate to={'/'}/>}/>
         </Routes>
     );
 };
